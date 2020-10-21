cd55945dc2d7a7922c83c9e286b4774d14d2ad3f
Filipe Lacerda <filipelacerda2661@gmail.com>
2020-10-21 20:04:45 -0300

    Adicionando os controller, services e repository no LocalizaçãoCarroModule, e adicionando o mesmo no app Module

--- ford-economic-workspace/ford-economic-api/api/src/app.module.ts
+++ ford-economic-workspace/ford-economic-api/api/src/app.module.ts
@@ -4,9 +4,10 @@ import { GrupoFamiliarModule } from './features/grupo-familiar/grupo-familiar.mo
 import { typeOrmConfig } from './config/typeormConfig.config'
 import { UsuarioModule } from './features/usuario/usuario.module';
 import { SensorModule } from './features/sensor/sensor.module';
+import { LocalizacaoCarroModule } from './features/localizacao-carro/localizacao-carro.module';
 
 
 @Module({
-  imports: [GrupoFamiliarModule, SensorModule, UsuarioModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [GrupoFamiliarModule, SensorModule, UsuarioModule, LocalizacaoCarroModule,TypeOrmModule.forRoot(typeOrmConfig)],
 })
 export class AppModule {}
--- ford-economic-workspace/ford-economic-api/api/src/features/localizacao-carro/localizacao-carro.module.ts
+++ ford-economic-workspace/ford-economic-api/api/src/features/localizacao-carro/localizacao-carro.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LocalizacaoCarrosController } from './localizacao-carro.controller';
+import { LocalizacaoCarroRepository } from './localizacao-carro.repository';
+import { LocalizacaoCarroService } from './localizacao-carro.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([LocalizacaoCarroRepository])],
+  controllers: [LocalizacaoCarrosController],
+  providers: [LocalizacaoCarroService],
 })
 export class LocalizacaoCarroModule {}
