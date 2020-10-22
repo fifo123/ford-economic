0fdeef61892fc13dcd5803f8190551d291908dfc
Filipe Lacerda <filipelacerda2661@gmail.com>
2020-10-21 21:03:52 -0300

    Renomeando carroService

--- ford-economic-workspace/ford-economic-api/api/src/features/carro/carro.controller.ts
+++ ford-economic-workspace/ford-economic-api/api/src/features/carro/carro.controller.ts
@@ -1,33 +1,34 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, ValidationPipe } from '@nestjs/common';
 import { CarroService } from './carro.service';
+import { AtualizarCarroDto } from './dto/atualizar-carro.dto';
 import { CarroDto } from './dto/carro.dto';
 
 @Controller('carro')
 export class CarroController {
-    constructor(private readonly usuarioService: CarroService){}
+    constructor(private readonly carroService: CarroService){}
 
     @Post('/criar')
     async criarCarro(@Body(ValidationPipe) data: CarroDto) {
-        return this.usuarioService.criarCarro(data);
+        return this.carroService.criarCarro(data);
     }
 
     @Get('')
     async listarCarros(){
-        return this.usuarioService.listarCarros();
+        return this.carroService.listarCarros();
     }
 
     @Get('/:id')
     async listarCarro(@Param('id') id: number){
-        return this.usuarioService.listarCarro(id);
+        return this.carroService.listarCarro(id);
     }
 
     @Patch('/atualizar/:id')
-    async atualizarCarro(@Param('id') id:number, @Body(ValidationPipe) data: CarroDto){
-        return this.usuarioService.atualizarCarro(id, data)
+    async atualizarCarro(@Param('id') id:number, @Body(ValidationPipe) data: AtualizarCarroDto){
+        return this.carroService.atualizarCarro(id, data)
     }
 
     @Delete('/deletar/:id')
     async deletarCarro(@Param('id') id:number){
-        return this.usuarioService.deletarCarro(id);
+        return this.carroService.deletarCarro(id);
     }
 }
