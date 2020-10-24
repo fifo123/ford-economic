f48eb01291c5c7f14050bd31e7b2541ec9c13b07
Filipe Lacerda <filipelacerda2661@gmail.com>
2020-10-24 13:59:02 -0300

    Criando estrutura básica das rotas

---
+++ ford-economic-workspace/ford-economic-front/ford-economic-mobile/src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+const Login: React.FC = () =>{
+    return(
+        <View>
+            <Text>Login - Ford Economic - dois</Text>
+        </View>
+    );
+}
+
+export default Login;
