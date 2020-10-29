d7f0a9555aab4fcb3eac0bdf4a363d6d1c1d74d1
Filipe Lacerda <filipelacerda2661@gmail.com>
2020-10-29 02:17:50 -0300

    Removendo a pasta files do git, e aprimorando o retorno de arquivos quando algo falhar

--- ford-economic-workspace/ford-economic-api/api/src/app.controller.ts
+++ ford-economic-workspace/ford-economic-api/api/src/app.controller.ts
@@ -4,9 +4,11 @@ import { Controller, Get, Param, Res } from '@nestjs/common';
 export class AppController {
 	@Get('get-image/:imagem')
 	getImagem(@Param('imagem') imagem: string, @Res() res: any) {
+		if (imagem == 'not_found.svg')
+			return res.sendFile('not_found.svg', { root: './assets' });
 		return res.sendFile(imagem, { root: './files' }, (err: any) => {
 			if (err) {
-				res.sendFile('not_found.svg', { root: './files' });
+				res.sendFile('not_found.svg', { root: './assets' });
 			}
 		});
 	}
