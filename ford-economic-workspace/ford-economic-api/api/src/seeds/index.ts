65faed57fe650d950f90f783e36f7c998d1f5952
Filipe Lacerda <filipelacerda2661@gmail.com>
2020-10-30 12:35:32 -0300

    Adicionando seeds das caracteristicas

--- ford-economic-workspace/ford-economic-api/api/src/seeds/index.ts
+++ ford-economic-workspace/ford-economic-api/api/src/seeds/index.ts
@@ -1,5 +1,7 @@
 import { SensorEntity } from 'src/features/sensor/sensor.entity';
+import { CaracteristicaEntity } from 'src/features/your-ford/caracteristica/caracteristica.entity';
 import { getRepository } from 'typeorm';
+import { valoresCaracteristicas } from './caracteristicas.seed';
 import { Seed } from './seeds.entity';
 import { valoresSensores } from './sensores.seed';
 
@@ -15,6 +17,19 @@ export async function seed() {
 				await sensoresRepository.save(valor);
 			});
 			await seedRepository.save({ seed: 'sensores' });
+		}
+
+		const caracteristicaSeed = await seedRepository.find({
+			where: { seed: 'caracteristica' },
+		});
+		if (caracteristicaSeed.length == 0) {
+			const caracteristicaRepository = getRepository<
+				CaracteristicaEntity
+			>('Caracteristica');
+			valoresCaracteristicas.map(async valor => {
+				await caracteristicaRepository.save(valor);
+			});
+			await seedRepository.save({ seed: 'caracteristica' });
 		}
 	} catch (error) {
 		throw error;
