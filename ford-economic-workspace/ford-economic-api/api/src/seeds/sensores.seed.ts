9965471223d1b33d88c468bd796092db8f68656f
Filipe Lacerda <filipelacerda2661@gmail.com>
2020-11-01 15:35:37 -0300

    Alterando entidade sensor e sua seed

--- ford-economic-workspace/ford-economic-api/api/src/seeds/sensores.seed.ts
+++ ford-economic-workspace/ford-economic-api/api/src/seeds/sensores.seed.ts
@@ -4,25 +4,31 @@ export const valoresSensores: SensorDto[] = [
 	{
 		descricao: 'Sensor de Ar-Condicionado',
 		nome: 'Ar-Condicionado',
+		icone: '../assets/ar-condicionado.svg',
 	},
 	{
 		descricao: 'Sensor para medição de quilometragem',
 		nome: 'Quilometragem',
+		icone: '../assets/direcao.svg',
 	},
 	{
 		descricao: 'Sensor para medição de nível de Combustível',
 		nome: 'Combustível',
+		icone: '../assets/combustivel.svg',
 	},
 	{
 		descricao: 'Sensor de Frenagem',
 		nome: 'Frenagem',
+		icone: '../assets/frenagem.svg',
 	},
 	{
 		descricao: 'Sensor para medir emissão de gases',
 		nome: 'Gases',
+		icone: '../assets/gases.svg',
 	},
 	{
 		descricao: 'Sensor do Motor',
 		nome: 'Motor',
+		icone: '../assets/motor.svg',
 	},
 ];
