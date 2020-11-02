8f585a834c783c9ab0de793a36befec191a93f56
Filipe Lacerda <filipelacerda2661@gmail.com>
2020-11-02 00:23:54 -0300

    Deletando constantes não usadas

--- ford-economic-workspace/ford-economic-api/api/src/features/ocorrencia-sensor/constants/valores-sensores.constants.ts
+++
@@ -1,2 +0,0 @@
-export const onParametro = `"OcorrenciaSensor"."criado" >= current_timestamp - '1 minute 30 second'::interval`;
-export const offParametro = `"OcorrenciaSensor"."criado" < current_timestamp - '1 minute 30 second'::interval`;
