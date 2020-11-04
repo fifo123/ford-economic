b0b7f68911bf072275b9d83130d878d32f52e453
Filipe Lacerda <filipelacerda2661@gmail.com>
2020-11-04 00:35:16 -0300

    Criando Interfaces para as request no backend do your-ford

---
+++ ford-economic-workspace/ford-economic-front/ford-economic-mobile/src/pages/YourFord/interface/your-ford-caracteristica.interface.ts
@@ -0,0 +1,7 @@
+export interface CaracteristicaYourFord {
+  id?: number;
+  nome?: string;
+  icone?: string;
+  atualizado?: string;
+  criado?: string;
+}
---
+++ ford-economic-workspace/ford-economic-front/ford-economic-mobile/src/pages/YourFord/interface/your-ford-modelo.interface.ts
@@ -0,0 +1,7 @@
+export interface ModeloYourFord {
+  id?: number;
+  nome?: string;
+  imagem?: string;
+  atualizado?: string;
+  criado?: string;
+}
