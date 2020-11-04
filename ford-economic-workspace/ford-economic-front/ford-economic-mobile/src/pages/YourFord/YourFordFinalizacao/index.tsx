b5e742aa769f1f77a578777824aa9b722e6736bf
Filipe Lacerda <filipelacerda2661@gmail.com>
2020-11-04 00:38:56 -0300

    Salvando no banco de dados o your ford criado

--- ford-economic-workspace/ford-economic-front/ford-economic-mobile/src/pages/YourFord/YourFordFinalizacao/index.tsx
+++ ford-economic-workspace/ford-economic-front/ford-economic-mobile/src/pages/YourFord/YourFordFinalizacao/index.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FordHeader from "../../../components/Shared/Header";
 import LabelTitulo from "../../../components/Shared/Titulo";
 import {
@@ -17,10 +17,54 @@ import {
   YourFordFinalizacaoGrid,
 } from "./styles";
 
-import FotoCarro from "../../../assets/carro.jpg";
-import FotoAr from "../../../assets/arcondicionado.svg";
+import api from "../../../services";
+import { YourFordCriar } from "../interface/your-ford-criar.interface";
+import { useHistory } from "react-router-dom";
 
 const YourFordFinalizacao: React.FC = () => {
+  const [nome, setNome] = useState<string>("");
+  const history = useHistory();
+  async function handleSubmit() {
+    if (nome == "") {
+      const nomeinput = document.getElementById("nome") as HTMLElement;
+      nomeinput.style.borderBottomColor = "var(--darkred)";
+    } else {
+      await api
+        .post<YourFordCriar, any>("/your-ford/criar", {
+          nome,
+          caracteristicaPrincipal: +(localStorage.getItem(
+            "principalId"
+          ) as string),
+          caracteristicaSecundaria1: +(localStorage.getItem(
+            "secundaria1Id"
+          ) as string),
+          caracteristicaSecundaria2: +(localStorage.getItem(
+            "secundaria2Id"
+          ) as string),
+          usoCarro1: +(localStorage.getItem("uso1Id") as string),
+          usoCarro2: +(localStorage.getItem("uso2Id") as string),
+          modelo: +(localStorage.getItem("modeloId") as string),
+          usuario: 1,
+        })
+        .then((response) => {
+          console.log(response.data);
+          localStorage.removeItem("principalIcone");
+          localStorage.removeItem("modeloImage");
+          localStorage.removeItem("principalId");
+          localStorage.removeItem("uso2Icone");
+          localStorage.removeItem("secundaria2Id");
+          localStorage.removeItem("uso2Id");
+          localStorage.removeItem("secundaria1Icone");
+          localStorage.removeItem("uso1Icone");
+          localStorage.removeItem("modeloId");
+          localStorage.removeItem("secundaria1Id");
+          localStorage.removeItem("secundaria2Icone");
+          localStorage.removeItem("uso1Id");
+          history.push("/your-ford-menu");
+        });
+    }
+  }
+
   return (
     <View>
       <YourFordFinalizacaoGrid>
@@ -31,11 +75,14 @@ const YourFordFinalizacao: React.FC = () => {
         />
         <DivNomeCriado>
           FORD
-          <CaixaDeTexto></CaixaDeTexto>
+          <CaixaDeTexto
+            id="nome"
+            onChange={(e) => setNome(e.target.value)}
+          ></CaixaDeTexto>
         </DivNomeCriado>
         <ImagemCarroFinalizacao>
           <img
-            src={FotoCarro}
+            src={localStorage.getItem("modeloImage") as string}
             style={{
               width: "100%",
               height: "100%",
@@ -48,7 +95,9 @@ const YourFordFinalizacao: React.FC = () => {
             <DivCaracteristicas>
               <DivCaracteristicaPrincipal>
                 <img
-                  src={FotoAr}
+                  src={`http://localhost:3000/get-image/?imagem=${
+                    localStorage.getItem("principalIcone") as string
+                  }`}
                   style={{
                     width: "100%",
                     height: "100%",
@@ -58,7 +107,9 @@ const YourFordFinalizacao: React.FC = () => {
               </DivCaracteristicaPrincipal>
               <DivCaracteristicaSecundaria>
                 <img
-                  src={FotoAr}
+                  src={`http://localhost:3000/get-image/?imagem=${
+                    localStorage.getItem("secundaria1Icone") as string
+                  }`}
                   style={{
                     width: "100%",
                     height: "100%",
@@ -68,7 +119,9 @@ const YourFordFinalizacao: React.FC = () => {
               </DivCaracteristicaSecundaria>
               <DivCaracteristicaSecundaria>
                 <img
-                  src={FotoAr}
+                  src={`http://localhost:3000/get-image/?imagem=${
+                    localStorage.getItem("secundaria2Icone") as string
+                  }`}
                   style={{
                     width: "100%",
                     height: "100%",
@@ -81,7 +134,9 @@ const YourFordFinalizacao: React.FC = () => {
               <DivUsoCarro>
                 {" "}
                 <img
-                  src={FotoAr}
+                  src={`http://localhost:3000/get-image/?imagem=${
+                    localStorage.getItem("uso1Icone") as string
+                  }`}
                   style={{
                     width: "100%",
                     height: "100%",
@@ -92,7 +147,9 @@ const YourFordFinalizacao: React.FC = () => {
               <DivUsoCarro>
                 {" "}
                 <img
-                  src={FotoAr}
+                  src={`http://localhost:3000/get-image/?imagem=${
+                    localStorage.getItem("uso2Icone") as string
+                  }`}
                   style={{
                     width: "100%",
                     height: "100%",
@@ -102,7 +159,9 @@ const YourFordFinalizacao: React.FC = () => {
               </DivUsoCarro>
             </DivUsos>
           </CaracteristicasContainerFinalizacao>
-          <BotaoCadastrarYourFord>Cadastrar Carro</BotaoCadastrarYourFord>
+          <BotaoCadastrarYourFord onClick={handleSubmit}>
+            Cadastrar Carro
+          </BotaoCadastrarYourFord>
         </ContainerFinalizacao>
       </YourFordFinalizacaoGrid>
     </View>
